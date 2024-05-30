a68961f14d52e32ec5181396338798f1dc95c9bf
Jaimin H Jungi <jj@gmail.com>
2024-05-30 18:42:47 +0530

    update

--- src/Component/Utility/API.js
+++ src/Component/Utility/API.js
@@ -1,5 +1,6 @@
 // const Baseurl="http://192.168.1.208:52202/"
-const Baseurl = "http://192.168.1.245:52202/"
+// const Baseurl = "http://192.168.1.245:52202/"
+const Baseurl = "http://103.250.147.184:52202/"
 const BaseurlCommon="http://103.131.196.61:42202/"
 
 // const Baseurl ="http://103.131.196.61:52202/"
